4b0c0aba030aa5a0f65a6a527ccd8a9bee24a5be
samroodAli <devsamrood@gmail.com>
2023-03-31 23:20:53 +0530

    feat: which command

---
+++ which.sh
@@ -0,0 +1,8 @@
+# which takes in the name of a program/executable as argument and shows you 
+# the past where the executable resides
+
+which ls # /bin/ls -- this is where the ls program resides
+
+# This will fail for shell built in commands such as pwd
+
+which pwd # fails as pwd is a shell built-in command
