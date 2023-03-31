84ddfe7042e5f302213dd27a99c2f10f401ad0b2
samroodAli <devsamrood@gmail.com>
2023-03-31 23:09:31 +0530

    refactor: add readme and add notes you can find on the course website

--- linux.sh
+++
@@ -1,7 +0,0 @@
-# Linux is very much a file system oriented operating sytem. Everything is a file in linux
-
-# it's kinda like explorer on windows or finder on mac, in linux, you are always in a directory
-
-# you can run pwd anywhere which will show the present working directory
-
-pwd # show the present working directory
