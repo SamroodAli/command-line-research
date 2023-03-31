368d1a8221a0d29ddb7fae4912cddf714bc2420b
samroodAli <devsamrood@gmail.com>
2023-03-31 22:56:47 +0530

    fix: typo in linux.sh

--- linux.sh
+++ linux.sh
@@ -1,4 +1,4 @@
-# Linux is very much a file system oriented. Everything is a file in linux
+# Linux is very much a file system oriented operating sytem. Everything is a file in linux
 
 # it's kinda like explorer on windows or finder on mac, in linux, you are always in a directory
 
