ec951583ec97a998b265f0a002b12291f77e9272
samroodAli <devsamrood@gmail.com>
2023-03-31 22:55:35 +0530

    feat: linux, the file based operating system

---
+++ linux.sh
@@ -0,0 +1,7 @@
+# Linux is very much a file system oriented. Everything is a file in linux
+
+# it's kinda like explorer on windows or finder on mac, in linux, you are always in a directory
+
+# you can run pwd anywhere which will show the present working directory
+
+pwd # show the present working directory
