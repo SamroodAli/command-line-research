cc558b6c7008c12f103023df318cc1b9c13b01ee
samroodAli <devsamrood@gmail.com>
2023-04-02 18:27:03 +0530

    feat: copy commands with cp

---
+++ cp:copying.sh
@@ -0,0 +1,20 @@
+# Copy a file from one location to another
+cp /path/to/source/file.txt /path/to/destination/
+
+# Copy a directory and its contents recursively
+cp -r /path/to/source/directory/ /path/to/destination/
+
+# Copy a file and preserve its attributes, such as timestamps and permissions
+cp -p /path/to/source/file.txt /path/to/destination/
+
+# Copy a file and force overwrite if the destination file already exists
+cp -f /path/to/source/file.txt /path/to/destination/
+
+# Copy a file and prompt before overwriting if the destination file already exists
+cp -i /path/to/source/file.txt /path/to/destination/
+
+# Copy multiple files into a directory
+cp /path/to/source/file1.txt /path/to/source/file2.txt /path/to/destination/
+
+# Copy all files in a directory into another directory
+cp /path/to/source/* /path/to/destination/
