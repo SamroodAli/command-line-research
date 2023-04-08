bcc3eedb380b5a4150db530ea34ee6e8ee53b1da
SamroodAli <samrood@peoplegrove.com>
2023-04-08 14:49:54 +0530

    feat: remove files and directories with rm

---
+++ rm:removing-files-and-directories.sh
@@ -0,0 +1,14 @@
+# we can remove files with rm
+rm file1 file2
+
+# we can use regex
+
+rm fil* # deletes all files starting with fil
+
+# delete directories with rm -rf
+# -r recursively
+# -f delete even if a file is not empty 
+
+rm -rf  # delete directories recursively
+
+rm -rf directory
