a1bcf81646f227dbaa20980db352b9decc03fe5b
SamroodAli <samrood@peoplegrove.com>
2023-04-08 14:17:22 +0530

    feat: get help manual for a utility with man

---
+++ man:manual-help.sh
@@ -0,0 +1,8 @@
+# man stands for manual and shows the manual for most utilities
+# example
+
+man less # would show the manual for "less"
+
+# if an app doesn't have a man page, try --help
+
+less --help
