d1372de3e9acb4318cedf1264525218d04e9b25d
SamroodAli <samrood@peoplegrove.com>
2023-04-08 14:45:01 +0530

    feat: create directories with mkdir

---
+++ mkdir:making-directories.sh
@@ -0,0 +1,10 @@
+# mkdir lets you create directories
+
+mkdir foo # creates a foo folder
+
+# the -p flag lets you create nested directories
+
+mkdir -p foo/bar/buzz # creates a foo directory, bar inside foo and buzz inside bar
+# foo
+#    bar
+#       buzz
