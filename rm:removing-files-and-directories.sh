b89ae82f47439eb9fb57a1d8d8a926ea7202d1a6
SamroodAli <samrood@peoplegrove.com>
2023-04-08 15:40:14 +0530

    feat: add wildcards, patterns and escaping characters

--- rm:removing-files-and-directories.sh
+++ rm:removing-files-and-directories.sh
@@ -1,10 +1,6 @@
 # we can remove files with rm
 rm file1 file2
 
-# we can use regex
-
-rm fil* # deletes all files starting with fil
-
 # delete directories with rm -rf
 # -r recursively
 # -f delete even if a file is not empty 
