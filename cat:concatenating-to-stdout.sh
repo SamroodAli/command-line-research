86aa5ab49ceecc30b175b88c21a8b3e1ac9ee3e1
SamroodAli <samrood@peoplegrove.com>
2023-04-08 14:24:15 +0530

    feat: concatenate something to stdout with cat

---
+++ cat:concatenating-to-stdout.sh
@@ -0,0 +1,5 @@
+# cat is often used to append short files to stdout
+# basically read short files,  use less for long ones
+# cat stands for concatenate as it is contaneting whatever we give to stdout
+
+cat someFile
