f04324d1398bf91fea2d13ae07e11bbc66eedef4
samroodAli <devsamrood@gmail.com>
2023-04-13 22:09:05 +0530

    feat: adding user specific and non user specific environment variables

--- environment-variables.sh
+++ environment-variables.sh
@@ -6,3 +6,18 @@ printenv
 
 NAME=samrood
 echo my name is $NAME # will log my name is samrood
+
+
+# These are not permanent but is only available in their session
+# But if we want them to be permanent, we can add them /etc/environment
+# But this will be available for every user
+sudo vim /etc/environment # open environment file
+
+# How to add user specific environment variables ?
+# we can add them in our shell rc file such as .bashrc, .zshrc etc
+# but you need
+vim  ~/.bashrc
+vim ~/.zshrc
+
+# example
+export SOME_VARIABLE="some value";
