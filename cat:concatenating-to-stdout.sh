2afa3ce71d85c61460c194a0b9aa9f975bbe3a54
SamroodAli <samrood@peoplegrove.com>
2023-05-28 02:07:26 +0530

    feat: reading files with echo rather than cat

--- cat:concatenating-to-stdout.sh
+++ cat:concatenating-to-stdout.sh
@@ -3,3 +3,8 @@
 # cat stands for concatenate as it is contaneting whatever we give to stdout
 
 cat someFile
+
+# update:
+# cat is not really meant to read files, but to merge ('concatenate') files 
+# together. One suggestion is to echo with stream redirection
+echo "$(< someFile)"
