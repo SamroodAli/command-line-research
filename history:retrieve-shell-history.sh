83cad93f4d07d6727afc248ffca20384af0a1ac7
SamroodAli <samrood@peoplegrove.com>
2023-05-28 09:53:01 +0530

    feat: add history command

---
+++ history:retrieve-shell-history.sh
@@ -0,0 +1,10 @@
+# We know every command we enter in an comand shell is often saved in a file.
+# The history command lists our previous commands
+# history is also a built in command in bash
+
+# list all previous commands
+history
+
+# shell saves all executed commands but there is a trick to save a command without
+# executing it. Save it as a comment!. Comments in bash start with '#'. Adding a command
+# in command shell saves it but isn't executed.
